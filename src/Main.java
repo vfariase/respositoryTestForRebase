b19848e4cf8e67c6fd27b9341414cc2ca854b929
vfariase <vfariase81@gmail.com>
2025-06-13 23:48:44 -0400

    add: implement toString method and update main greeting

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
+
+    @Override
+    public String toString(String saludo) {
+        return saludo;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world sample 1!");
+
     }
 }
